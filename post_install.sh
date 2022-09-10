3db0d80de127cf60642e61a3dab9d2a9b7b7dd07
DiamondPete <87245367+diamondpete@users.noreply.github.com>
2022-09-09 21:41:43 -0400

    Fix Order

--- post_install.sh
+++ post_install.sh
@@ -4,14 +4,15 @@
 
 ln -s /usr/local/bin/python3.9 /usr/local/bin/python
 
-/usr/local/bin/python -m venv $MYLAR_APP_DIR
-$MYLAR_APP_DIR/python -m pip install --upgrade pip
-$MYLAR_APP_DIR/pip install user_agent2
-
 # Install mylar
 pw useradd mylar -u 5479 -s /bin/csh -m
 git clone https://github.com/mylar3/mylar3.git $MYLAR_APP_DIR
 cd $MYLAR_APP_DIR || { echo "Clone Mylar failed."; exit 1; }
+
+/usr/local/bin/python -m venv $MYLAR_APP_DIR
+$MYLAR_APP_DIR/bin/python -m pip install --upgrade pip
+$MYLAR_APP_DIR/bin/pip install user_agent2
+
 mkdir /usr/ports
 chown -R mylar:mylar $MYLAR_APP_DIR
 
