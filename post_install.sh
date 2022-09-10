ec65eeb97fd61fe82b9e6503af3c2153e13f5217
DiamondPete <87245367+diamondpete@users.noreply.github.com>
2022-09-09 21:34:25 -0400

    Use Python Virtual Env

--- post_install.sh
+++ post_install.sh
@@ -4,14 +4,16 @@
 
 ln -s /usr/local/bin/python3.9 /usr/local/bin/python
 
-python -m pip install --upgrade pip
-pip install user_agent2
+/usr/local/bin/python -m venv $MYLAR_APP_DIR
+$MYLAR_APP_DIR/python -m pip install --upgrade pip
+$MYLAR_APP_DIR/pip install user_agent2
 
 # Install mylar
 pw useradd mylar -u 5479 -s /bin/csh -m
 git clone https://github.com/mylar3/mylar3.git $MYLAR_APP_DIR
 cd $MYLAR_APP_DIR || { echo "Clone Mylar failed."; exit 1; }
-chown -R mylar $MYLAR_APP_DIR
+mkdir /usr/ports
+chown -R mylar:mylar $MYLAR_APP_DIR
 
 sysrc -f /etc/rc.conf mylar_enable="YES"
 
